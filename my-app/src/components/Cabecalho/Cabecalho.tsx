eaae8a7a7ee1128b9ccdedb009d743b815f02a47
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-02 11:04:54 -0300

    feat: Componente recriado e implementado ao APP. Cabecalho

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,12 @@
+export default function Cabecalho() {
+    return(
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <nav>
+                <li>Item-1</li>
+                <li>Item-2</li>
+                <li>Item-3</li>
+            </nav>
+        </header>
+    )
+}
