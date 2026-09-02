097738aab2f28b9d08d6e33c3f3c552151b083cf
manuelalramos <manuela.lima.r@outlook.com>
2026-09-02 11:04:56 -0300

    feat : Cria arquivos base

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo(){
+    return(
+        <section>
+            <h2>Conteúdo</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, magnam. Laborum, voluptatem placeat. Similique natus minima quod ipsa magnam quia, iure quibusdam, provident quisquam corporis, tempora voluptas molestias vero eligendi.</p>
+        </section>
+    )
+}
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Conteudo(){
+    return(
+        <footer>
+            <p>&copy; Todos os direitos reservados. 2026</p>
+        </footer>
+    )
+}
