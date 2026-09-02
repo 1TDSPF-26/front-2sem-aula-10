9aebf3fbc7876e29fa580125ad1d0529727544bb
lenahlawi07 <lynnhalawi07@gmail.com>
2026-09-02 13:03:38 -0300

    feat : Componentes funcionais criados e integrados no APP. Cabecalho, Conteudo e Rodape. Imagens inseridas. Estrutura inical de useState.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,13 @@
+import Cabecalho from "./components/Cabecalho/cabecalho";
+import Conteudo from "./components/Conteudo/conteudo";
+import Rodape from "./components/Rodape/rodape";
+
 export default function App() {
   return (
-    <main>
-      <h1>Minha aplicação React</h1>
-    </main>
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
   )
 }
---
+++ my-app/src/components/Cabecalho/cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <h1>Meu Boiler Plate</h1>
+            <ul>
+                <li>Item-1</li>
+                <li>Item-2</li>
+                <li>Item-3</li>
+            </ul>
+        </header>
+    )
+}
