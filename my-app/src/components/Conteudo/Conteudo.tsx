b691aecba0757529d16cad8c13f5bf6ce6010eb1
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-02 12:03:41 -0300

    feat: Utilização do UseState para dinamicidade na alteração de valores.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import imgQuadrado from '../../img/quadrado.png';
 
 
 export default function Conteudo() {
+
+    let numeroComum = 0;
+    
+    //Estado do React
+    const [numeroState, setNumeroState] = useState(0);
+
+    function aumentaVariavelComum() {
+        numeroComum = numeroComum + 1;
+
+        // O valor muda e aparece no console
+        console.log("Variavel Comum: ", numeroComum);
+    }
+
+    function aumentarUseState() {
+        // Por ser uma const, criamos uma arrow function para alteracao do valor
+        setNumeroState((valorAtual)=> valorAtual + 1)
+
+        console.log("Valor do estado: ", numeroState);
+    }
     return(
         <main>
+
+            <div>
+                <h2>Exemplo de variável comum</h2>
+                <p>Variavel Comum: {numeroComum}</p>
+                <button onClick={aumentaVariavelComum}>Aumentar Variavel Comum</button>
+            </div>
+            <div>
+                <h2>Exemplo de UseState</h2>
+                <p>Valor do state: {numeroState}</p>
+                <button onClick={aumentarUseState}>Aumentar o valor do state</button>
+            </div>
+
             <section>
                 <h2>Conteudo</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, ducimus.</p>
