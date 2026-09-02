bfe27c0c34a1aaed32f706aecba439759ebe9bc2
MatheusRodriguesSerrao <rmatheusrodrigues193@gmail.com>
2026-09-02 11:04:50 -0300

    feat: Componentes funcionais criados e integrados ao APP. Cabecalho, Conteudo e Rodape.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
   )
 }
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,13 @@
+
+export default function Conteudo() {
+  return (
+    <main>
+        <section>
+            <h2>Conteúdo</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero perspiciatis expedita beatae, at tempora praesentium nihil fuga illum aut, maiores consequuntur porro repellendus sit laudantium, nemo explicabo modi molestiae ipsa?
+            </p>
+        </section>
+    </main>
+  )
+}
