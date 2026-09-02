4e39f496fb96b8f8e7e3c46a1e854b3e4e215b1a
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-09-02 11:02:44 -0300

    feat: Componentes funcionais criados e integrados ao APP. Cabecalho, Conteudo e Rodape.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo() {
+    return(
+        <body>
+            <h1>Conteudo</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, ducimus.</p>
+        </body>
+    )
+}
