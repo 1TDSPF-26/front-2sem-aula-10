a2adb7f6bad7390536ffec9887336e611a1f0c83
igorblacconaro <igorbs2910@gmail.com>
2026-09-02 11:03:39 -0300

    feat: Componentes funcionais básicos criados e integrados no App.

---
+++ my-app/src/components/Conteudo/index.tsx
@@ -0,0 +1,14 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <section>
+                <h2>Conteúdo</h2>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem deserunt magni nobis recusandae molestiae debitis assumenda vero incidunt unde dolore harum officiis consectetur commodi est, sunt obcaecati similique labore voluptatum?
+
+                </p>
+
+            </section>
+        </main>
+    );
+}
