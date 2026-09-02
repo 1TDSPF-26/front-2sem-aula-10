850074901b458a314a4b3a1ef1be0f36611a6148
lkoprowskif <larikoprowski@gmail.com>
2026-09-02 11:33:00 -0300

    feat: Exemplos utilizando imagens e referências diferentes.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,3 +1,5 @@
+import imgQuadrado from "../../img/quadrado.png";
+
 export default function Conteudo() {
   return (
     <main>
@@ -6,6 +8,27 @@ export default function Conteudo() {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe aspernatur temporibus dolorum doloribus eaque. 
         Consequatur necessitatibus, voluptate odit facilis suscipit explicabo praesentium incidunt blanditiis fugit sequi, 
         quos aperiam tenetur ducimus!</p>
+      </section>
+      <section>
+        <h2>Imagem com link externo</h2>
+        <figure>
+          <img src="https://placehold.co/600x400/FFFFF0/FFFFFF/png" alt="Imagem de exemplo de 600x400px" />
+          <figcaption>Imagem de exemplo 600x400px</figcaption>
+        </figure>
+      </section>
+      <section>
+        <h2>Imagem com referência interna</h2>
+        <figure>
+          <img src={imgQuadrado} alt="Imagem quadrada 400x400px" />
+          <figcaption>Imagem de exemplo 400x400px</figcaption>
+        </figure>
+      </section>
+      <section>
+        <h2>Imagem com referência interna</h2>
+        <figure>
+          <img src="/image/lampada.png" alt="" />
+          <figcaption>Imagem de exemplo estática - Lâmpada</figcaption>
+        </figure>
       </section>
     </main>
   );
