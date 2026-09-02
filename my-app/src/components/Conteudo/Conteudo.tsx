fbc802955f5636106c165745720fb2ea0332690d
manuelalramos <manuela.lima.r@outlook.com>
2026-09-02 11:33:06 -0300

    feat : utilzando imagem e usando tipos de refencias diferentes

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,8 +1,35 @@
-export default function Conteudo(){
-    return(
+import imgQuadrado from "../../image/quadrado.png";
+
+export default function Conteudo() {
+  return (
+    <main>
         <section>
             <h2>Conteúdo</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, magnam. Laborum, voluptatem placeat. Similique natus minima quod ipsa magnam quia, iure quibusdam, provident quisquam corporis, tempora voluptas molestias vero eligendi.</p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero perspiciatis expedita beatae, at tempora praesentium nihil fuga illum aut, maiores consequuntur porro repellendus sit laudantium, nemo explicabo modi molestiae ipsa?
+            </p>
         </section>
-    )
+        <section>
+            <h2>Imagem com link externo</h2>
+            <figure>
+              <img src="https://placehold.co/600x400/e1e1e1/000000/png" alt="Imagem de exemplo de 600x400px" />
+              <figcaption>Imagem de exemplo de 600x400px</figcaption>
+            </figure>
+        </section>
+        <section>
+          <h2>Imagem com referência interna</h2>
+          <figure>
+            <img src={imgQuadrado} alt="Imagem quadrada 400x400px" />
+            <figcaption>Imagem de exemplo 400x400px</figcaption>
+          </figure>
+        </section>
+        <section>
+          <h2>Imagem com referência interna estática</h2>
+          <figure>
+            <img src="/img/lampada.png" alt="Lâmpada de desenho" />
+            <figcaption>Imagem de exemplo estática - Lâmpada</figcaption>
+          </figure>
+        </section>
+    </main>
+  )
 }
