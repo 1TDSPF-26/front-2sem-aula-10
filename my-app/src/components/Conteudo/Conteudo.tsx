f851fc2636b5f8f86ebf66705342a41fe47bad9d
lkoprowskif <larikoprowski@gmail.com>
2026-09-02 12:09:19 -0300

    feat: Aprendendo a diferença entre uma variável comum dentro e fora do useState.

--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import imgQuadrado from "../../img/quadrado.png";
 
 export default function Conteudo() {
+
+    let numeroComum = 0;
+
+    //Estado do React
+    const [numeroState, setNumeroState] = useState(0);
+
+    function aumentaVariavelComum(){
+      numeroComum = numeroComum + 1;
+
+      //O valor nmuda e aparece no console
+      console.log("Variavel comum:", numeroComum)
+      //Mas não aparecerá na página
+    }
+
+    function aumentarUseState(){
+      //O React altera o estado e renderiza novamente a p[agina/componente]
+      setNumeroState((valorAtual)=> valorAtual + 1);
+      console.log("Valor do estado: ", numeroState)
+    }
+
   return (
     <main>
+
+      <div>
+        <h2>Exemplo de variável comum</h2>
+        <p>Variável comum: {numeroComum}</p>
+        <button onClick={aumentaVariavelComum}>Aumentar variável comum</button>
+      </div>
+      <div>
+        <h2>Exemplo de UseState</h2>
+        <p>Valor do state: {numeroState}</p>
+        <button onClick={aumentarUseState}>Aumentar o valor do state</button>
+      </div>
       <section>
         <h2>Conteúdo principal</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe aspernatur temporibus dolorum doloribus eaque. 
@@ -26,7 +58,7 @@ export default function Conteudo() {
       <section>
         <h2>Imagem com referência interna</h2>
         <figure>
-          <img src="/image/lampada.png" alt="" />
+          <img src="/image/lampada.png" alt="Lâmpada de Desenho" />
           <figcaption>Imagem de exemplo estática - Lâmpada</figcaption>
         </figure>
       </section>
