30848754280730bd25f02e8536f7a2d760766ff7
BryanC0staDev <bryancosta713@gmail.com>
2026-09-02 11:07:37 -0300

    feat: criação de componentes

---
+++ my-app/src/components/Cabecalho.tsx
@@ -0,0 +1,13 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Minha Aplicação</h1>
+      <h2>Bem-vindo à minha aplicação</h2>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </header>
+  )
+}
---
+++ my-app/src/components/Rodape.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Rodape() {
+  return (
+    <footer>
+        <p>&copy; 2026 Minha Aplicação. Todos os direitos reservados.</p>
+    </footer>
+  )
+}
