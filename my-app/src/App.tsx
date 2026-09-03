18bc6623e55a2490270dd7e600c2ff94d1e95391
ogmatheus7 <yudimatheus759@gmail.com>
2026-09-03 17:51:33 -0300

    feat: Adicionando imagens lampada.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,1 +1,13 @@
-export defalut function App
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
+
+export default function App() {
+  return (
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+      <Rodape/>
+    </div>
+  )
+}
